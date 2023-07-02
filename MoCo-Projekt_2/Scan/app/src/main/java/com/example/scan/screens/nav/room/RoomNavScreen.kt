f6cda459aea1dfe4e34948848c341fa6ae6d5ced
SueleymanOezel <74266172+SueleymanOezel@users.noreply.github.com>
2023-07-02 16:09:46 +0200

    so - GoogleMapsAPI wurde eingefügt und funktioniert per PREVIEW.

--- MoCo-Projekt_2/Scan/app/src/main/java/com/example/scan/screens/nav/room/RoomNavScreen.kt
+++ MoCo-Projekt_2/Scan/app/src/main/java/com/example/scan/screens/nav/room/RoomNavScreen.kt
@@ -1,59 +1,77 @@
 package com.example.scan.screens.nav.room
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.scan.R
 
-
+//OptIn: verwendete Material3-Bibliothek experimentell, Änderungen an der API können auftreten
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RoomNavScreen(roomText: String, roomBackgroundResId: Int) {
-    val roomBackground = painterResource(id = roomBackgroundResId)
+fun RoomNavScreen(context: Context) {
+    val startPoint = "Cologne"
+    val destination = "Berlin"
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Image(
-            painter = roomBackground,
-            contentDescription = "Hintergrundbild",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop,
-        )
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black.copy(alpha = 0.6f))
+        TextField(
+            value = startPoint,
+            onValueChange = { /* Keine Änderungen erlaubt */ },
+            label = { Text("Startpunkt eingeben") },
+            enabled = false // Textfeld deaktivieren
         )
 
-        Text(
-            text = roomText,
-            style = TextStyle (
-                fontSize = 20.sp,
-                color = Color.White,
-                fontWeight = FontWeight.Bold
-            ),
-            modifier = Modifier.padding(16.dp)
+        TextField(
+            value = destination,
+            onValueChange = { /* Keine Änderungen erlaubt */ },
+            label = { Text("Ziel eingeben") },
+            enabled = false // Textfeld deaktivieren
         )
+
+        Button(
+            onClick = {
+                startNavigation(startPoint, destination, context)
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Navigation starten")
+        }
+    }
+}
+
+fun startNavigation(startPoint: String, destination: String, context: Context) {
+    val gmmIntentUri = Uri.parse("google.navigation:q=$startPoint&destination=$destination&mode=w")
+    val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+    mapIntent.setPackage("com.google.android.apps.maps")
+
+    if (mapIntent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(mapIntent)
+    } else {
+        Toast.makeText(
+            context,
+            "Google Maps ist auf diesem Gerät nicht installiert.",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
 
 @Preview
 @Composable
 fun RoomNavScreenPreview() {
-    RoomDetailScreen(roomText = "Dies ist der Text für Raum 1234", roomBackgroundResId = R.drawable.logo)
+    val context = androidx.compose.ui.platform.LocalContext.current
+    RoomNavScreen(context = context)
 }
